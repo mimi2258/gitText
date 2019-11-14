53497dd32689626767e995262badd86a164a0821
clearlove7 <995149112@qq.com>
2019-11-14 17:00:06 +0800

    第一次添加工程

---
+++ src/cn/itcast/User.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class User {
+    public static void main(String[] args) {
+        for (int i = 0; i <3; i++) {
+            System.out.println("aaa");
+        }
+    }
+}
