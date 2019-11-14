d36ab4a068ad18534791abd15577b86152d4b582
clearlove7 <995149112@qq.com>
2019-11-14 18:50:08 +0800

    第二次修改

--- src/cn/itcast/User.java
+++ src/cn/itcast/User.java
@@ -1,9 +1,6 @@
 package cn.itcast;
 
 public class User {
-    public static void main(String[] args) {
-        for (int i = 0; i <3; i++) {
-            System.out.println("aaa");
-        }
-    }
+    private int id;
+    private String name="master";
 }
